91298c74b146d7fdaeb71315b4246a4d2f4078d3
amikalsen <amikalsen@hotmail.com>
2024-11-23 05:25:00 +0100

    add readme

--- start.ps1
+++
@@ -1,9 +0,0 @@
-# Start the server in the background
-Start-Process "node" "server.js"
-
-# Wait for the server to start
-Start-Sleep -Seconds 2
-
-# Open two private Firefox windows
-Start-Process "firefox.exe" -ArgumentList "-private-window http://localhost:3000"
-Start-Process "firefox.exe" -ArgumentList "-private-window http://localhost:3000"
